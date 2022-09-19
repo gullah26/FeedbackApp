4248c95d9429c427f9eba32ef0ed747ff1e31b60
Seun Ajetomobi <fullstackfory@gmail.com>
2022-09-19 10:05:06 +0000

    add style, state and card

---
+++ feedbackapp/src/data/FeedbackData.js
@@ -0,0 +1,66 @@
+const FeedbackData = [
+
+    {
+        id: 1,
+        rating: 10,
+        text: 'Lorem ipsum dolor sit amet conscuteor aproko la we dey goon of the moago finta emozium elezium cumbasum.',
+    },
+
+    {
+        id: 2,
+        rating: 9,
+        text: 'Lorem ipsum dolor sit amet conscuteor aproko la we dey goon of the moago finta emozium elezium cumbasum.',
+    },
+
+    {
+        id: 3,
+        rating: 8,
+        text: 'Lorem ipsum dolor sit amet conscuteor aproko la we dey goon of the moago finta emozium elezium cumbasum.',
+    },
+
+    {
+        id: 4,
+        rating: 7,
+        text: 'Lorem ipsum dolor sit amet conscuteor aproko la we dey goon of the moago finta emozium elezium cumbasum.',
+    },
+
+
+    {
+        id: 5,
+        rating: 6,
+        text: 'Lorem ipsum dolor sit amet conscuteor aproko la we dey goon of the moago finta emozium elezium cumbasum.',
+    },
+
+    {
+        id: 6,
+        rating: 5,
+        text: 'Lorem ipsum dolor sit amet conscuteor aproko la we dey goon of the moago finta emozium elezium cumbasum.',
+    },
+
+    {
+        id: 7,
+        rating: 4,
+        text: 'Lorem ipsum dolor sit amet conscuteor aproko la we dey goon of the moago finta emozium elezium cumbasum.',
+    },
+
+    {
+        id: 8,
+        rating: 3,
+        text: 'Lorem ipsum dolor sit amet conscuteor aproko la we dey goon of the moago finta emozium elezium cumbasum.',
+    },
+
+    {
+        id: 9,
+        rating: 2,
+        text: 'Lorem ipsum dolor sit amet conscuteor aproko la we dey goon of the moago finta emozium elezium cumbasum.',
+    },
+
+    {
+        id: 10,
+        rating: 1,
+        text: 'Lorem ipsum dolor sit amet conscuteor aproko la we dey goon of the moago finta emozium elezium cumbasum.',
+    }
+
+]
+
+export default FeedbackData;
